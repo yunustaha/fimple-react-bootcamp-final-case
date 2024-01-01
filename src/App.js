565404de0d1c168ddfa5651a4baccaff85fe031a
yunustahayilmaz <yunustahayilmaz@gmail.com>
2024-01-01 16:40:45 +0300

    added _redirects and some route fix.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import {
   createBrowserRouter,
+  Navigate,
   RouterProvider,
-  useNavigate,
 } from "react-router-dom";
 import ApplicationStatus from "./Pages/ApplicationStatus";
 import CreateApplication from "./Pages/CreateApplication";
@@ -10,67 +10,59 @@ import Application from "./Pages/Application";
 import Admin from "./Pages/Admin/Admin";
 import AdminApplication from "./Pages/Admin/Application";
 import ApplicationList from "./Pages/Admin/ApplicationList";
-import { useEffect } from "react";
 import PrivateRoute from "./components/PrivateRoute";
 import Layout from "./components/Layout";
 
-const RedirectFromRoot = () => {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    navigate("/basvuru-olustur");
-  }, [navigate]);
-
-  return null;
-};
-
 function App() {
-  const router = createBrowserRouter([
-    {
-      // parent route component
-      element: <Layout />,
-      // child route components
-      children: [
-        {
-          path: "/",
-          element: <RedirectFromRoot />,
-        },
-        {
-          path: "/basvuru-olustur",
-          element: <CreateApplication />,
-        },
-        {
-          path: "/basvuru-basarili/:insertId",
-          element: <ApplicationStatus />,
-        },
-        {
-          path: "/basvuru-sorgula",
-          element: <CheckApplication />,
-        },
-        {
-          path: "/basvuru/:id",
-          element: <Application />,
-        },
-        {
-          path: "/admin",
-          children: [
-            {
-              index: true,
-              element: <Admin />,
-            },
-            {
-              path: "basvuru/:id",
-              element: <PrivateRoute Component={AdminApplication} />,
-            },
-            {
-              path: "basvuru-listesi",
-              element: <PrivateRoute Component={ApplicationList} />,
-            },
-          ],
-        },
-      ],
-    },
-  ]);
+  const router = createBrowserRouter(
+    [
+      {
+        // parent route component
+        element: <Layout />,
+        // child route components
+        children: [
+          {
+            path: "/",
+            element: <Navigate to="/basvuru-olustur" />,
+          },
+          {
+            path: "/basvuru-olustur",
+            element: <CreateApplication />,
+          },
+          {
+            path: "/basvuru-basarili/:insertId",
+            element: <ApplicationStatus />,
+          },
+          {
+            path: "/basvuru-sorgula",
+            element: <CheckApplication />,
+          },
+          {
+            path: "/basvuru/:id",
+            element: <Application />,
+          },
+          {
+            path: "/admin",
+            children: [
+              {
+                index: true,
+                element: <Admin />,
+              },
+              {
+                path: "basvuru/:id",
+                element: <PrivateRoute Component={AdminApplication} />,
+              },
+              {
+                path: "basvuru-listesi",
+                element: <PrivateRoute Component={ApplicationList} />,
+              },
+            ],
+          },
+        ],
+      },
+    ],
+    { basename: "/" },
+  );
 
   return <RouterProvider router={router} />;
 }
